85b6ba65a79fce76d797e123870acafa83e9acf0
vitalikost <vitalikostrovskij@yandex.ru>
2017-11-01 11:23:07 +0200

    add record databases

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,4 +4,10 @@ class EmailsController < ApplicationController
     #render html:"<h1>Hello</h1>".html_safe
   end
 
+  def create
+    @address = Email.new(params.require(:email).permit(:address))
+    @address.save
+    redirect_to @address
+  end
+
 end
